c7943f35283d02837ddacaddb4b5850fdd7b10de
Garrett George <garrett.michael.george@gmail.com>
2021-09-25 16:24:27 -0500

    wip:feat: money formats as dummy string

--- lib/pos/money.ex
+++ lib/pos/money.ex
@@ -4,4 +4,9 @@ defmodule POS.Money do
   @type t :: %__MODULE__{
     cents_value: integer
   }
+
+  defimpl String.Chars, for: __MODULE__ do
+    def to_string(%POS.Money{} = money),
+      do: "#{money.cents_value}"
+  end
 end
--- test/pos/money_test.exs
+++ test/pos/money_test.exs
@@ -6,4 +6,9 @@ defmodule POS.MoneyTest do
   test "has an integer cents value" do
     assert %Money{cents_value: 123}
   end
+
+  test "format as string" do
+    money = %Money{cents_value: 9999}
+    assert to_string(money)
+  end
 end
