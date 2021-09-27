8443c28568a3ea61c958bcc8f4f1de0fe6096af9
Garrett George <garrett.michael.george@gmail.com>
2021-09-27 08:26:26 -0500

    wip: scaffold POS.handle_barcode/1

--- lib/pos.ex
+++ lib/pos.ex
@@ -1,2 +1,5 @@
 defmodule POS do
+  def handle_barcode(barcode_string) do
+    true
+  end
 end
--- test/pos_test.exs
+++ test/pos_test.exs
@@ -3,4 +3,10 @@ defmodule POSTest do
   doctest POS
 
   # TODO: make a test list!
+  describe "handling a barcode" do
+    test "on successful product lookup, sends product data to display" do
+      barcode_string = "123"
+      assert POS.handle_barcode(barcode_string)
+    end
+  end
 end
